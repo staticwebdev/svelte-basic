df3bfe25d0cfb8fe21cf2d31ef26fb1b22dbba12
Hannah Zhu <t-hannzhu@microsoft.com>
2022-06-16 15:28:59 -0700

    changed the start command for package.json

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,7 +73,7 @@ const config: PlaywrightTestConfig = {
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    port: 3000,
+    port: 5000,
   }
 };
 
